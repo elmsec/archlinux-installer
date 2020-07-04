e2af382f6deb824a1960050f49e5142bf413485d
canelma <e.canelma@gmail.com>
2020-07-04 15:47:56 +0300

    do not stop execution when a command fails

--- installer/install.sh
+++ installer/install.sh
@@ -6,13 +6,13 @@
 . ./installer/helpers.sh
 
 
-# exit when any command fails
-set -e
+# # exit when any command fails
+# set -e
 
-# keep track of the last executed command
-trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
-# echo an error message before exiting
-trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+# # keep track of the last executed command
+# trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# # echo an error message before exiting
+# trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
 
 # set a keyboard layout
